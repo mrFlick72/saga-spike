f00e5581200ba753caf124d0c84fbf625e86e8be
mrFlick72 <valerio.vaudi@gmail.com>
2019-10-30 20:59:58 +0100

    move endpoint trigger on web app

--- salesorder-service/src/main/kotlin/it/valeriovaudi/sagaspike/salesorderservice/CatalogMessaging.kt
+++ salesorder-service/src/main/kotlin/it/valeriovaudi/sagaspike/salesorderservice/CatalogMessaging.kt
@@ -11,7 +11,6 @@ import org.springframework.messaging.SubscribableChannel
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Flux
-import reactor.core.publisher.toFlux
 import reactor.core.publisher.toMono
 import java.io.Serializable
 import java.math.BigDecimal
@@ -51,8 +50,14 @@ class CatalogMessagingListeners {
     }
 }
 
-data class CatalogGoods(var barcode: String, var name: String) : Serializable
+data class CatalogGoods(var barcode: String, var name: String) : Serializable {
+    constructor() : this("", "")
+}
 
-data class CatalogPrice(var price: BigDecimal, var currency: String) : Serializable
+data class CatalogPrice(var price: BigDecimal, var currency: String) : Serializable {
+    constructor() : this(BigDecimal.ZERO, "")
+}
 
-data class CatalogGoodsWithPrice(var goods: CatalogGoods, var price: CatalogPrice) : Serializable
+data class CatalogGoodsWithPrice(var goods: CatalogGoods, var price: CatalogPrice) : Serializable {
+    constructor() : this(CatalogGoods(), CatalogPrice())
+}
--- webapp/src/main/kotlin/it/valeriovaudi/sagaspike/webapp/SalesOrderEndPoint.kt
+++ webapp/src/main/kotlin/it/valeriovaudi/sagaspike/webapp/SalesOrderEndPoint.kt
@@ -1,16 +1,21 @@
-package it.valeriovaudi.sagaspike.salesorderservice
+package it.valeriovaudi.sagaspike.webapp
 
 import org.springframework.messaging.support.MessageBuilder
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import reactor.core.publisher.Mono
+import java.util.*
 
 @RestController
 class SalesOrderEndPoint(private val salesOrderMessageChannel: SalesOrderMessageChannel) {
 
     @PostMapping("/sales-order")
-    fun createSaleOrder(@RequestBody createSalesOrderRequest: CreateSalesOrderRequest){
-        val build = MessageBuilder.withPayload(createSalesOrderRequest).build()
+    fun createSaleOrder(@RequestBody createSalesOrderRequest: CreateSalesOrderRequest): Mono<String> {
+        val executionId = UUID.randomUUID().toString()
+        val build = MessageBuilder.withPayload(createSalesOrderRequest).setHeader("execution-id", executionId).build()
         salesOrderMessageChannel.createSalesOrderRequestChannel().send(build)
+
+        return Mono.just(executionId)
     }
 }
