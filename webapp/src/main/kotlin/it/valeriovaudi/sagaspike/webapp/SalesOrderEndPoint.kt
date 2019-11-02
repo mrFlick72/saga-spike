734ff0f672ab6aaa3b4fc40316ce5b181d4a7fb6
mrFlick72 <valerio.vaudi@gmail.com>
2019-11-02 16:57:39 +0100

    happy path end point!

--- webapp/src/main/kotlin/it/valeriovaudi/sagaspike/webapp/SalesOrderEndPoint.kt
+++ webapp/src/main/kotlin/it/valeriovaudi/sagaspike/webapp/SalesOrderEndPoint.kt
@@ -1,21 +1,57 @@
 package it.valeriovaudi.sagaspike.webapp
 
-import org.springframework.messaging.support.MessageBuilder
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.integration.annotation.Gateway
+import org.springframework.integration.annotation.MessagingGateway
+import org.springframework.integration.dsl.IntegrationFlows
+import org.springframework.integration.dsl.MessageChannels
+import org.springframework.messaging.handler.annotation.Payload
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
-import reactor.core.publisher.Mono
 import java.util.*
 
 @RestController
-class SalesOrderEndPoint(private val salesOrderMessageChannel: SalesOrderMessageChannel) {
+class SalesOrderEndPoint(private val newSalesOrderGateway: NewSalesOrderGateway) {
 
     @PostMapping("/sales-order")
-    fun createSaleOrder(@RequestBody createSalesOrderRequest: CreateSalesOrderRequest): Mono<String> {
-        val executionId = UUID.randomUUID().toString()
-        val build = MessageBuilder.withPayload(createSalesOrderRequest).setHeader("execution-id", executionId).build()
-        salesOrderMessageChannel.createSalesOrderRequestChannel().send(build)
+    fun createSaleOrder(@RequestBody createSalesOrderRequest: CreateSalesOrderRequest) =
+            newSalesOrderGateway.newSalesOrder(createSalesOrderRequest)
+}
 
-        return Mono.just(executionId)
-    }
+@MessagingGateway
+interface NewSalesOrderGateway {
+
+    @Gateway(requestChannel = "newSalesOrderRequestChannel", replyChannel = "newSalesOrderResponseChannel")
+    fun newSalesOrder(@Payload createSalesOrderRequest: CreateSalesOrderRequest): String
+}
+
+@Configuration
+class NewSalesOrderPipelineConfig {
+
+    @Bean
+    fun newSalesOrderRequestChannel() = MessageChannels.publishSubscribe()
+
+    @Bean
+    fun newSalesOrderResponseChannel() = MessageChannels.direct()
+
+    @Bean
+    fun newSalesOrderErrorChannel() = MessageChannels.direct()
+
+
+    @Bean
+    fun newSalesOrderIdEchoPipeline(salesOrderMessageChannel: SalesOrderMessageChannel) =
+            IntegrationFlows.from(newSalesOrderRequestChannel())
+                    .transform { source: CreateSalesOrderRequest -> source.salesOrderId }
+                    .channel(newSalesOrderResponseChannel())
+                    .get()
+
+    @Bean
+    fun newSalesOrderSagaProcessingPipeline(salesOrderMessageChannel: SalesOrderMessageChannel) =
+            IntegrationFlows.from(newSalesOrderRequestChannel())
+                    .enrichHeaders(mapOf("execution-id" to UUID.randomUUID().toString()))
+                    .channel(salesOrderMessageChannel.createSalesOrderRequestChannel())
+                    .get()
+
 }
